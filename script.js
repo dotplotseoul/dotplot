2777c96569c2e925f3927c73540532af3162fe06
dotplotseoul <dotplotstudio@gmail.com>
2023-03-10 13:12:28 +0900

    test

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hi");
+console.clear();
+
+$('.dot_second').click(function(){
+  $('.main_bar').toggleClass('showback');
+  $(this).toggleClass('big');
+  $('.dot_first').toggleClass('small');
+});
+
+const $cursor = $('.cursor');
+
+$(window).mousemove(function(e) {
+  $cursor.css({
+    top:e.clientY,
+    left:e.clientX
+  });
+});
+
+$('.cursor-big').mouseenter(function(){
+    $('.cursor').addClass('need-to-cursor-big'),
+    $('.cursor').css('background-color', 'red')
+});
+
+$('.cursor-big').mouseleave(function(){
+    $('.cursor').removeClass('need-to-cursor-big')
+    $('.cursor').css('background-color', 'white')
+});
